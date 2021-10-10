8cb9c9c93ddd377b42d542de3c1bb0bbf8f1e414
jannielynacha <acha.jannielyn@gmail.com>
2021-10-11 07:11:47 +0800

    Add Review and Critic models and index template

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Critic(models.Model):
+	display_name = models.CharField(max_length=100)
+	sort_name = models.CharField(max_length=100)
+	status = models.CharField(max_length=10)
+	bio = models.CharField(max_length=300)
+	seo_name = models.CharField(max_length=200)
+
+
+class Review(models.Model):
+	display_title = models.CharField(max_length=5000)
+	mpaa_rating = models.CharField(max_length=100)
+	critics_pick = models.IntegerField()
+	byline = models.ForeignKey(Critic, on_delete=models.CASCADE)
+	headline = models.CharField(max_length=400)
+	summary_short = models.CharField(max_length=500)
+	publication_date = models.CharField(max_length=100)
+	opening_date = models.CharField(max_length=100)
+	date_updated = models.CharField(max_length=100)
+	review_text = models.CharField(max_length=200)
+	bookmark = models.ManyToManyField(User, related_name='bookmark', blank=True)
+
+class Link(models.Model):
+	review = models.OneToOneField(Review, on_delete=models.CASCADE, primary_key=True)
+	type = models.CharField(max_length=100)
+	url = models.CharField(max_length=500)
+	suggested_link_text = models.CharField(max_length=500)
+
+
+class Multimedia(models.Model):
+	review = models.OneToOneField(Review, on_delete=models.CASCADE, primary_key=True)
+	type = models.CharField(max_length=100)
+	src = models.CharField(max_length=500)
+	height = models.IntegerField()
+	width = models.IntegerField()
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from .models import Review, Critic, Link, Multimedia
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	current_user = request.user
+	review_list = Review.objects.order_by('-publication_date')[:5]
+	template = loader.get_template('reviews/index.html')
+	context = {
+		'review_list': review_list,
+		'user': current_user
+	}
+	return HttpResponse(template.render(context, request))
