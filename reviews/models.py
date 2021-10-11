5ae051abfc9a48579885ae59bf34a4d48083721d
jannielynacha <acha.jannielyn@gmail.com>
2021-10-11 21:29:53 +0800

    Modify templates

--- reviews/models.py
+++ reviews/models.py
@@ -4,23 +4,22 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Critic(models.Model):
 	display_name = models.CharField(max_length=100)
-	sort_name = models.CharField(max_length=100)
-	status = models.CharField(max_length=10)
-	bio = models.CharField(max_length=300)
+	sort_name = models.CharField(max_length=100, blank=True)
+	status = models.CharField(max_length=10, blank=True)
+	bio = models.CharField(max_length=300, blank=True)
 	seo_name = models.CharField(max_length=200)
 
 
 class Review(models.Model):
-	display_title = models.CharField(max_length=5000)
-	mpaa_rating = models.CharField(max_length=100)
-	critics_pick = models.IntegerField()
-	byline = models.ForeignKey(Critic, on_delete=models.CASCADE)
-	headline = models.CharField(max_length=400)
-	summary_short = models.CharField(max_length=500)
-	publication_date = models.CharField(max_length=100)
-	opening_date = models.CharField(max_length=100)
-	date_updated = models.CharField(max_length=100)
-	review_text = models.CharField(max_length=200)
+	display_title = models.CharField(max_length=5000, blank=True)
+	mpaa_rating = models.CharField(max_length=100, blank=True)
+	critics_pick = models.IntegerField(blank=True)
+	byline = models.CharField(max_length=500, blank=True)
+	headline = models.CharField(max_length=400, blank=True)
+	summary_short = models.CharField(max_length=500, blank=True)
+	publication_date = models.CharField(max_length=100, blank=True)
+	opening_date = models.CharField(max_length=100, null=True)
+	date_updated = models.CharField(max_length=100, blank=True)
 	bookmark = models.ManyToManyField(User, related_name='bookmark', blank=True)
 
 class Link(models.Model):
